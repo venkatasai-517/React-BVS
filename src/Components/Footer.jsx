eb467a80e86651467e832871c5ad0f854682956c
venkatasai-517 <20k65a0517@sasi.ac.in>
2024-06-13 11:54:50 +0530

    footer changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,110 +1,198 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const Footer = () => {
   return (
-    <>
-      <div className="text-white">
-        <footer className="bg-dark p-5 ">
-          <div className="row ">
-            <div className="col-6 col-md-2 mb-3  ">
-              <h5 className="">CONTENT</h5>
-              <ul className="nav flex-column ">
-                <li className="nav-item mb-2  ">
-                  <a href="#" className="nav-link p-0 text-white ">
-                    Festivals
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0  text-white">
-                    Trending
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0  text-white">
-                    Blog
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-6 col-md-2 mb-3  ">
-              <h5>LEGAL</h5>
-              <ul className="nav flex-column">
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link  text-white p-0 ">
-                    Terms of use
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link  text-white p-0 ">
-                    License
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link  text-white p-0 ">
-                    Privacy Policy
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-6 col-md-2 mb-3  ">
-              <h5>Support</h5>
-              <ul className="nav flex-column">
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0  text-white ">
-                    FAQ
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0  text-white ">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-md-5 offset-md-1 mb-3">
-              <form className="">
-                <h5>Subscribe to our newsletter</h5>
-                <p>Monthly digest of whats new and exciting from us.</p>
-                <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                  <label className="visually-hidden">Email address</label>
-                  <input
-                    id="newsletter1"
-                    type="text"
-                    className="form-control"
-                    placeholder="Email address"
-                  ></input>
-                  <button className="btn btn-primary" type="button">
-                    Subscribe
-                  </button>
-                </div>
-              </form>
-            </div>
+    <div className="text-white">
+      <footer className="bg-dark p-5">
+        <div className="row">
+          <div className="col-12 col-md-4 mb-3">
+            <h5>BUSINESS VICTORY SOLUTIONS</h5>
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-white">
+                  Website Development
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-white">
+                  Mobile App Development
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-white">
+                  Advertising, Ads Designing
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-white">
+                  Social Media Promotion
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-white">
+                  Brand & Logo Designing
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link p-0 text-white">
+                  Celebrity Promotion
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle p-0 text-white"
+                  href="#"
+                  role="button"
+                  id="dropdownMenuLink"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Digital Media Marketing
+                </a>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuLink"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Google Ads
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Facebook Ads
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Instagram Ads
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      YouTube Ads
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Website Ads
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      addtheadd.com ads
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </div>
 
-          <div className="d-flex flex-column flex-sm-row justify-content-between border-top ">
-            <p>&copy; 2022 Company, Inc. All rights reserved.</p>
-            <ul className="list-unstyled d-flex">
-              <li className="ms-3">
-                <a className="link" href="#">
-                  <i className="bi bi-instagram"></i>
+          <div className="col-12 col-md-5 mb-3">
+            <h5>CONTACT DETAILS</h5>
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <a href="#" className="nav-link text-white p-0">
+                  business victory solutions
                 </a>
               </li>
-              <li className="ms-3">
-                <a className="link" href="#">
-                  <i className="bi bi-twitter-x"></i>
+              <li className="nav-item mb-2">
+                Manager contact:
+                <a href="tel:+918523899330" className="nav-link text-white p-0">
+                  8523899330
+                </a>
+                <a href="tel:+917993558948" className="nav-link text-white p-0">
+                  7993558948
                 </a>
               </li>
-              <li className="ms-3">
-                <a className="link" href="#">
-                  <i className="bi bi-facebook"></i>
+              <li className="nav-item mb-2">
+                Email:
+                <a
+                  href="mailto:bvsadds@gmail.com"
+                  className="nav-link text-white p-0"
+                >
+                  bvsadds@gmail.com
                 </a>
               </li>
             </ul>
           </div>
-        </footer>
-      </div>
-    </>
+
+          <div className="col-12 col-md-3 mb-3">
+            <h5>SOCIAL MEDIA</h5>
+            <div className="d-flex">
+              <a href="#" className="text-white me-3">
+                <i className="fa-brands fa-facebook fa-3x"></i>
+              </a>
+              <a href="#" className="text-white me-3">
+                <i className="fa-brands fa-square-instagram fa-3x"></i>
+              </a>
+              <a href="#" className="text-white">
+                <i className="fa-brands fa-linkedin fa-3x"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="d-flex flex-column flex-sm-row justify-content-between border-top pt-3">
+          <p>&copy; 2024BVS Company, Inc. All rights reserved.</p>
+          <ul className="list-unstyled d-flex">
+            <li className="ms-3">
+              <a className="link-light" href="#">
+                <i className="bi bi-instagram"></i>
+              </a>
+            </li>
+            <li className="ms-3">
+              <a className="link-light" href="#">
+                <i className="bi bi-twitter"></i>
+              </a>
+            </li>
+            <li className="ms-3">
+              <a className="link-light" href="#">
+                <i className="bi bi-facebook"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom">
+        <div className="container-fluid">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center w-100">
+            <li className="nav-item mx-3">
+              <a className="nav-link disabled">
+                <i className="fa-solid fa-arrow-left"></i>
+              </a>
+            </li>
+            <li className="nav-item mx-3">
+              <a className="nav-link active" aria-current="page" href="#">
+                <i className="fas fa-home"></i>
+              </a>
+            </li>
+            <li className="nav-item mx-3">
+              <a className="nav-link" href="#">
+                <i className="fas fa-store"></i>
+              </a>
+            </li>
+            <li className="nav-item mx-3">
+              <a className="nav-link" href="#">
+                <i className="fas fa-search"></i>
+              </a>
+            </li>
+            <li className="nav-item mx-3">
+              <a className="nav-link disabled">
+                <i className="fas fa-user"></i>
+              </a>
+            </li>
+            <li className="nav-item mx-3">
+              <a className="nav-link disabled">
+                <i className="fa-solid fa-arrow-right"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 };
+
 export default Footer;
